d47824b2b11d8f7617b7c6f04bfa04d67b355b96
Aditya Chavan <127530500+cadityab09@users.noreply.github.com>
2024-08-22 19:31:29 +0530

    button changed

--- lib/veiw/home_screen.dart
+++ lib/veiw/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:queens_problem/veiw/queen_problems_4.dart';
 import 'package:queens_problem/veiw/queen_problems_8.dart';
 
@@ -13,80 +12,74 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: ()async{
-        SystemNavigator.pop();
-        return true;
-      },
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Queens Problem', style: TextStyle(color: Colors.white),),
-          centerTitle: true,
-          backgroundColor: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Queens Problem',
+          style: TextStyle(color: Colors.white),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Center(
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => QueenProblems4()),
-                  );
-                },
-                child: Container(
-                  width: 250, // Width of the button
-                  height: 60, // Height of the button
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: Colors.blue, // Background color
-                    borderRadius: BorderRadius.circular(12), // Rounded corners
-                  ),
-                  child: Text(
-                    '4-Queens Problem',
-                    style: TextStyle(
-                      color: Colors.white, // Text color
-                      fontSize: 20, // Text size
-                      fontWeight: FontWeight.bold, // Text weight
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 20,),
-            Center(
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => QueenProblems8()),
-                  );
-                },
-                child: Container(
-                  width: 250, // Width of the button
-                  height: 60, // Height of the button
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    color: Colors.blue, // Background color
-                    borderRadius: BorderRadius.circular(12), // Rounded corners
-                  ),
-                  child: Text(
-                    '8-Queens Problem',
-                    style: TextStyle(
-                      color: Colors.white, // Text color
-                      fontSize: 20, // Text size
-                      fontWeight: FontWeight.bold, // Text weight
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-
+        centerTitle: true,
+        backgroundColor: Colors.blue,
       ),
-    );;
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12), // Rounded corners
+                ),
+                backgroundColor: Colors.blue, // Background color
+              ),
+              onPressed: () async{
+                await Future.delayed(Duration(seconds: 1));
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const QueenProblems4()),
+                );
+              },
+              child: const Text(
+                '4-Queens Problem',
+                style: TextStyle(
+                  color: Colors.white, // Text color
+                  fontSize: 20, // Text size
+                  fontWeight: FontWeight.bold, // Text weight
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          Center(
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12), // Rounded corners
+                ),
+                backgroundColor: Colors.green, // Background color
+              ),
+              onPressed: () async{
+                await Future.delayed(Duration(seconds: 1));
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const QueenProblems8()),
+                );
+              },
+              child: const Text(
+                '8-Queens Problem',
+                style: TextStyle(
+                  color: Colors.white, // Text color
+                  fontSize: 20, // Text size
+                  fontWeight: FontWeight.bold, // Text weight
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
